9cca7a0dd66829250e99a165c5bedfb6707e5807
Алмас Вильданов <almasvildanoff@yandex.ru>
2024-10-06 13:59:07 +0300

    changes after 2nd lesson

---
+++ lyceum/homepage/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase, Client
+
+
+class StaticURLTests(TestCase):
+    def test_home_page_endpoint(self):
+        # Делаем запрос к главной странице и проверяем статус
+        responde = Client().get("/")
+        # Утверждаем, что для прохождения теста код должен быть равен 200
+        self.assertEqual(responde.status_code, 200)
