0fd243c45dbc2d35f7be7d9f58152f843912b75c
Алмас Вильданов <almasvildanoff@yandex.ru>
2024-10-06 19:16:17 +0300

    fixes

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -10,6 +10,6 @@ class StaticURLTests(TestCase):
 
     def test_catalog_page_detail_endpoint(self):
 
-        response = Client().get("/catalog/1")
+        response = Client().get("/catalog/1/")
 
         self.assertEqual(response.status_code, 200, "ERROR-WRONG RESPONSE!")
--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "default_secret_key")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fake_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG_ENV = os.getenv("DJANGO_DEBUG", "true").lower()
