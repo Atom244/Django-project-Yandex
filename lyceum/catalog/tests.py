2d65ca1eda797fadb9ad6541e3e3c4bf230e2983
Алмас Вильданов <almasvildanoff@yandex.ru>
2024-10-10 20:04:56 +0300

    исправление issue 2

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -5,7 +5,6 @@ from parameterized import parameterized
 
 class StaticURLTests(TestCase):
     def test_catalog_page_endpoint(self):
-
         normal_catalog_check = Client().get("/catalog/")
         self.assertEqual(
             normal_catalog_check.status_code, 200, "normal_catalog_check down"
@@ -16,51 +15,36 @@ class StaticURLTests(TestCase):
             wrong_catalog_check.status_code, 404, "wrong_catalog_check down"
         )
 
-    @parameterized.expand([(-1), (1.5), (0)])
-    def test_catalog_pages(self, parameter):
+    @parameterized.expand([("",), ("/re",), ("/converter",)])
+    def test_catalog(self, point):
+        parameters = [-1, 1.5, 0]
 
-        normal_catalog_check = Client().get("/catalog/1/")
-        self.assertEqual(
-            normal_catalog_check.status_code, 200, "normal_catalog_check down."
-        )
-
-        normal_catalog_re_check = Client().get("/catalog/re/1/")
-        self.assertEqual(
-            normal_catalog_re_check.status_code,
-            200,
-            "normal_catalog_re_check down",
-        )
-
-        normal_catalog_converter_check = Client().get("/catalog/converter/1/")
-        self.assertEqual(
-            normal_catalog_converter_check.status_code,
-            200,
-            "normal_catalog_convrter_check down",
-        )
-
-        negative_catalog_re_check = Client().get("/catalog/re/{paramter}/")
-        self.assertEqual(
-            negative_catalog_re_check.status_code,
-            404,
-            f"negative_catalog_re_check down. Parameter: {parameter}",
-        )
-
-        negative_catalog_converter_check = Client().get(
-            f"/catalog/converter/{parameter}/"
-        )
-        self.assertEqual(
-            negative_catalog_converter_check.status_code,
-            404,
-            f"negative_catalog_converter_check down. Parameter: {parameter}",
-        )
-        if parameter == 0:
-            self.skipTest(
-                "Следующий тест пропущен, "
-                "тк по адресу /catalog/<param> допустим 0"
+        for parameter in parameters:
+            normal_catalog_check = Client().get(f"/catalog{point}/1/")
+            self.assertEqual(
+                normal_catalog_check.status_code,
+                200,
+                f"normal_catalog_check down. Parameter: {point}",
             )
-        negative_catalog_check = Client().get(f"/catalog/{parameter}/")
-        self.assertEqual(
-            negative_catalog_check.status_code,
-            404,
-            "negative_catalog_check down",
-        )
+
+            if parameter == 0:
+                self.skipTest(
+                    "Следующий тест пропущен, "
+                    "тк по адресу /catalog/<param> допустим 0"
+                )
+
+            negative_catalog_check = Client().get(
+                f"/catalog{point}/{parameter}/"
+            )
+            self.assertEqual(
+                negative_catalog_check.status_code,
+                404,
+                f"negative_catalog_check down. Parameter: {parameter}",
+            )
+
+            negative_catalog_check = Client().get(f"/catalog/{parameter}/")
+            self.assertEqual(
+                negative_catalog_check.status_code,
+                404,
+                f"negative_catalog_check down. Parameter: {parameter}",
+            )
