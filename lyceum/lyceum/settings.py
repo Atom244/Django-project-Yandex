47e7b4e92fa272fc74938ef07bd9c1142787f642
Алмас Вильданов <almasvildanoff@yandex.ru>
2024-10-25 14:30:43 +0300

    фиксы для з4 л4

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -89,7 +89,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "lyceum.context_processors.current_year",
             ],
         },
     },
