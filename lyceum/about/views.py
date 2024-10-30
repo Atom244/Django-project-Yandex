14d2a9d756cf94f1253fba21e624df71a7e48571
Алмас Вильданов <almasvildanoff@yandex.ru>
2024-10-30 17:10:24 +0300

    убрал контекст в вьюс где не надо

--- lyceum/about/views.py
+++ lyceum/about/views.py
@@ -5,5 +5,4 @@ __all__ = ["description"]
 
 def description(request):
     template = "about/about.html"
-    context = {}
-    return render(request, template, context)
+    return render(request, template)
