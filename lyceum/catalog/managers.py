fd2a03348452b0806381bb5dd2ba9f55e2db0c96
Алмас Вильданов <almasvildanoff@yandex.ru>
2024-11-03 23:09:15 +0300

    (Lecture 6 #2) 2

--- lyceum/catalog/managers.py
+++ lyceum/catalog/managers.py
@@ -6,27 +6,16 @@ __all__ = []
 
 
 class ItemManager(django.db.models.Manager):
-    def published(self):
-        queryset = self.get_queryset()
-        published_items = self._filter_published(queryset)
-        published_items = self._select_related(published_items)
-        published_items = self._prefetch_related(published_items)
-
-        return published_items.only(
-            "name",
-            "text",
-            "category__name",
-            "main_image__image",
-        )
-
-    def _filter_published(self, queryset):
+    def get_queryset(self):
+        queryset = super().get_queryset()
         return queryset.filter(is_published=True, category__is_published=True)
 
-    def _select_related(self, queryset):
-        return queryset.select_related("category", "main_image")
-
-    def _prefetch_related(self, queryset):
-        return queryset.prefetch_related(
+    def published(self):
+        queryset = self.get_queryset()
+        queryset = queryset.select_related(
+            "category",
+            "main_image",
+        ).prefetch_related(
             django.db.models.Prefetch(
                 "tags",
                 queryset=catalog.models.Tag.objects.published().defer(
@@ -34,11 +23,20 @@ class ItemManager(django.db.models.Manager):
                 ),
             ),
         )
+        return queryset.only(
+            "name",
+            "text",
+            "category__name",
+            "main_image__image",
+        )
 
     def on_main(self):
         return self.published().filter(is_on_main=True).order_by("name")
 
 
 class PublishedTagManager(django.db.models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
+
     def published(self):
-        return self.get_queryset().filter(is_published=True)
+        return self.get_queryset()
