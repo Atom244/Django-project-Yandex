4ac0e5e15a732f286e16c812c1251b4fe9bc86f3
Алмас Вильданов <almasvildanoff@yandex.ru>
2024-11-03 16:45:06 +0300

    (Lecture 6 #1) 5

--- lyceum/catalog/tests/test_urls.py
+++ lyceum/catalog/tests/test_urls.py
@@ -4,10 +4,36 @@ from django.test import Client, TestCase
 from django.urls import NoReverseMatch, reverse
 from parameterized import parameterized
 
+import catalog.models
+
 __all__ = []
 
 
 class StaticURLTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        category = catalog.models.Category.objects.create(
+            name="Тестовая категория",
+            slug="test-category",
+            weight=10,
+        )
+
+        tag = catalog.models.Tag.objects.create(
+            name="Тестовый тег",
+            slug="test-tag",
+        )
+
+        cls.item = catalog.models.Item.objects.create(
+            name="Тестовый товар",
+            text="Это описание товара, содержащее роскошно и превосходно.",
+            category=category,
+            is_on_main=True,
+        )
+
+        cls.item.tags.add(tag)
+
+        cls.client = Client()
+
     def test_catalog_page_endpoint(self):
         norm_url = reverse("catalog:item-list")
         normal_catalog_check = Client().get(norm_url)
@@ -28,17 +54,17 @@ class StaticURLTest(TestCase):
         [
             (-1, HTTPStatus.NOT_FOUND),
             (1.5, HTTPStatus.NOT_FOUND),
-            (-0, HTTPStatus.OK),
+            (-0, HTTPStatus.NOT_FOUND),
             ("1FH#@KKl", HTTPStatus.NOT_FOUND),
             ("0lff9", HTTPStatus.NOT_FOUND),
             ("&hge59", HTTPStatus.NOT_FOUND),
             ("$h78", HTTPStatus.NOT_FOUND),
             (1, HTTPStatus.OK),
-            (10, HTTPStatus.OK),
-            (100, HTTPStatus.OK),
-            (0, HTTPStatus.OK),
+            (10, HTTPStatus.NOT_FOUND),
+            (100, HTTPStatus.NOT_FOUND),
+            (0, HTTPStatus.NOT_FOUND),
             ("01", HTTPStatus.OK),
-            ("010", HTTPStatus.OK),
+            ("010", HTTPStatus.NOT_FOUND),
         ],
     )
     def test_catalog(self, parameter, code):
