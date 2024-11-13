4bc5a2e1b14511a2a674e6e3faaaee0d3255ed62
Алмас Вильданов <almasvildanov@gmail.com>
2024-11-13 17:58:47 +0300

    (L7 #3) 4

--- lyceum/users/backends.py
+++ lyceum/users/backends.py
@@ -11,14 +11,13 @@ __all__ = []
 
 class LoginBackend(django.contrib.auth.backends.ModelBackend):
     def authenticate(self, request, username=None, password=None):
-        user = None
-
         try:
             validate_email(username)
             is_email = True
         except ValidationError:
             is_email = False
 
+        user = None
         if is_email:
             try:
                 user = users.models.ProxyUser.objects.by_mail(username)
@@ -51,9 +50,3 @@ class LoginBackend(django.contrib.auth.backends.ModelBackend):
             user.profile.send_reactivation_email(request)
 
         return None
-
-    def get_user(self, user_id):
-        try:
-            return users.models.ProxyUser.objects.get(pk=user_id)
-        except users.models.ProxyUser.DoesNotExist:
-            return None
